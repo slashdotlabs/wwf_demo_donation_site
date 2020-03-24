a55b2e495d3251f2cbfa7d9d86a1015ce23e197d
steekam <waynewanyee@gmail.com>
2020-03-24 20:40:10 +0300

    fix: indentation in markdown rendering as code block

--- resources/views/mail/account_setup.blade.php
+++ resources/views/mail/account_setup.blade.php
@@ -1,15 +1,15 @@
 @component('mail::message')
-    # Dear {{ $donor->last_name }},
+# Dear {{ $donor->last_name }},
 
-    Thank you for supporting the cause and contributing to the
-    WWF Kenyan Chapter.
+Thank you for supporting the cause and contributing to the
+WWF Kenyan Chapter.
 
-    Setup your account to access your details and monitor your contributions.
+Setup your account to access your details and monitor your contributions.
 
-    @component('mail::button', ['url' => $setup_url])
-        Setup Your Account
-    @endcomponent
+@component('mail::button', ['url' => $setup_url])
+    Setup Your Account
+@endcomponent
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
